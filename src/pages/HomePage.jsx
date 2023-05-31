c7ce5f266a084e13e9fbb82286c23c1b75af6c00
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-05-31 13:51:07 +0200

    updated homepage content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,50 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import ProductList from '../components/ProductList';
 
-function ProductList() {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const response = await fetch('https://api.noroff.dev/api/v1/online-shop');
-        if (!response.ok) {
-          throw new Error('Failed to fetch products');
-        }
-        const productsData = await response.json();
-        console.log(productsData);
-        setProducts(productsData.slice(0, 50)); // Limit to first 50 products
-      } catch (error) {
-        setIsError(true);
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchProducts();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error occurred while fetching the products.</div>;
-  }
-
+function HomePage() {
   return (
     <div>
-      <h2>Products List</h2>
-      {products.map((product) => (
-        <div key={product.id}>
-          <Link to={`/product/${product.id}`}>{product.title || 'Untitled'}</Link> {/* Update the link URL */}
-        </div>
-      ))}
+      <h1>Welcome to the Home Page</h1>
+      <ProductList />
     </div>
   );
 }
 
-export default ProductList;
+export default HomePage;
