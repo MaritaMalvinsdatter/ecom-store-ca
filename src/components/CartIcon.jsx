a140b12e1b99ee5b4dee2d1777d980cf3626b209
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-11-07 14:49:55 +0100

    usenvigate to checkout the cart

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,11 +1,18 @@
 // CartIcon.jsx
 import React from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
 
 const CartIcon = ({ itemCount }) => {
+  const navigate = useNavigate();
+
+  const goToCheckout = () => {
+    navigate('/checkout');
+  };
+
   return (
-    <div className={styles.cartIcon}>
+    <div className={styles.cartIcon} onClick={goToCheckout}>
       <FaShoppingCart size={24} />
       {itemCount > 0 && (
         <span className={styles.itemCount}>
