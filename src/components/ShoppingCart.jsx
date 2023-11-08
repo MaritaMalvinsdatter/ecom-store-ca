8a00a281628eac8114abe1cd25ca426e96984ad2
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-11-08 09:52:43 +0100

    set cart and add to cart function

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,7 +1,11 @@
-// ShoppingCart.js
+
 import React, { createContext, useState, useContext } from 'react';
 
-const CartContext = createContext();
+const CartContext = createContext({
+  cart: [],
+  setCart: () => {},
+  addToCart: () => {},
+});
 
 export const useCart = () => useContext(CartContext);
 
@@ -10,22 +14,19 @@ export const CartProvider = ({ children }) => {
 
   const addToCart = (product) => {
     setCart((prevCart) => {
-      // Check if the product is already in the cart
       const productExists = prevCart.find(item => item.id === product.id);
       if (productExists) {
-        // Increase quantity
         return prevCart.map(item =>
           item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
         );
       } else {
-        // Add new item
         return [...prevCart, { ...product, quantity: 1 }];
       }
     });
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, setCart, addToCart }}>
       {children}
     </CartContext.Provider>
   );
