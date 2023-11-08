1ab2353be5d087d1e1b42b9fb6785678ad8aa194
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-11-08 09:54:01 +0100

    chechout success page

--- src/pages/CheckoutSuccessPage.jsx
+++ src/pages/CheckoutSuccessPage.jsx
@@ -1,10 +1,14 @@
+// CheckoutSuccessPage.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CheckoutSuccessPage = () => {
   return (
     <div>
-      <h2>Checkout Success Page</h2>
-      {/* Add checkout success page content */}
+      <h1>Order Successful!</h1>
+      <p>Thank you for shopping with us today. Your order has been placed and is being processed.</p>
+      <p>If you enjoyed your shopping experience here, please tell your friends about us! </p>
+      <Link to="/">Back to Store</Link>
     </div>
   );
 };
