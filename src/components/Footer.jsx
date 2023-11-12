982f352982e3ad27764f1c3726962fb3acff5fd2
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-11-12 13:47:13 +0100

    added margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer className='text-center'> &copy; e-Come store</footer>;
+  return <footer className='text-center mt-5'> &copy; e-Come store</footer>;
 };
 
 export default Footer;
